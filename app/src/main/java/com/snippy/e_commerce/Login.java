d4ba9c459d49232811f51dcfa1b61dffe1a8497b
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-10 19:56:26 +0530

    added new login page

---
+++ app/src/main/java/com/snippy/e_commerce/Login.java
@@ -0,0 +1,6 @@
+package com.snippy.e_commerce;
+
+import androidx.fragment.app.Fragment;
+
+public class Login extends Fragment {
+}
--- app/src/main/java/com/snippy/e_commerce/MainActivity.java
+++ app/src/main/java/com/snippy/e_commerce/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        if(savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction().add(R.id.container,new Login()).commit();
+        }
     }
 }
